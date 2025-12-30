c853b1ca6965f7e0ee8d4f57aefd8d740247d3ec
sina <sina301973@gmail.com>
2025-12-30 13:51:31 +0330

    add function to display a message with user name

--- app1/urls.py
+++ app1/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from app1 import views
 urlpatterns = [
-    path("hello/", views.hello_world)
+    path("hello/", views.hello_world),
+    path("hello2/", views.hello_world2),
 ]
--- app1/views.py
+++ app1/views.py
@@ -3,3 +3,10 @@ from rest_framework.response import Response
 @api_view(['GET'])
 def hello_world(request):
     return Response({"message": "Hello, how are you?!"})
+
+@api_view(['GET', 'POST'])
+def hello_world2(request):
+    if request.method == 'GET':
+        return Response({"message": "Hello, how are you?!"})
+    elif request.method == 'POST':
+        return Response({"message": "Hello, how are you {} ?!".format(request.data["name"])})
