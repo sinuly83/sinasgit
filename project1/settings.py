e70b1e37eff30989fdc5e9c5f4f7dc61ee220c1d
sina <sina301973@gmail.com>
2026-01-05 20:06:45 +0330

    created the employee application

--- project1/settings.py
+++ project1/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'app1',
     'rest_framework',
+    'employee',
 ]
 
 MIDDLEWARE = [
--- project1/urls.py
+++ project1/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path , include
 
+import employee
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('first_project/', include('app1.urls')),
+    path('employee/',include('employee.urls'))
 ]
